d9ca7e2f357fedff3e6882e4ba95952505e062a4
mayukhdev <mayukhdev@gmail.com>
2016-04-24 12:16:47 +0530

    Removed api key

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var wagner = require('wagner-core');
+var path = require('path');
 
 var app = express();
 
 //Create API key
-var key = require('./apikey')(15);
+var key = "";
+//var key = require('./apikey')(15);
 
 require('./models')(wagner);
 require('./Database/dependencies')(wagner);
@@ -15,5 +17,10 @@ var api_url =  '/api/v1/' + key;
 
 app.use(api_url, require('./api')(wagner));
 
+var rootpath = path.normalize(__dirname + '/../');
+console.log("Static: " + rootpath + 'www');
+
+app.use(express.static(rootpath + '/www'));
+
 app.listen(process.argv[2] || 3000);
-console.log('Listening on port!');
+console.log('Listening on port 3000!');
